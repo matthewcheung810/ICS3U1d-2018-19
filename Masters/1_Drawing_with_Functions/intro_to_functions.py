3a6ec3942f11ae71596d86208f97d8433a92ae33
robuntu <robuntu@stro.ycdsb.ca>
2019-03-04 11:07:30 -0500

    Monday 4th, March 2019, 11:06pm
    Hey Matthew, try and stay awake.

--- Masters/1_Drawing_with_Functions/intro_to_functions.py
+++ Masters/1_Drawing_with_Functions/intro_to_functions.py
@@ -5,6 +5,20 @@ SCREEN_HEIGHT = 600
 
 # define your draw functions
 
+def draw_cloud():
+    """ Draw a cloud """
+
+    #cloud
+    arcade.draw_ellipse_filled(650, 500, 80, 80, arcade.color.WHITE, 0)
+    arcade.draw_ellipse_filled(570, 480, 55, 50, arcade.color.WHITE, 0)
+    arcade.draw_ellipse_filled(730, 460, 50, 50, arcade.color.WHITE, 0)
+
+def draw_rollinghills():
+    """ Draw rolling hills """
+
+    #rolling hills
+    arcade.draw_ellipse_filled(600, -10, 300, 100, arcade.color.ISLAMIC_GREEN, 0)
+    arcade.draw_ellipse_filled(100, -200, 500, 400, arcade.color.KELLY_GREEN, 0)
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
@@ -12,6 +26,8 @@ def main():
     arcade.start_render()
 
    # call your draw functions
+    draw_cloud()
+    draw_rollinghills()
 
     # Finish and run
     arcade.finish_render()
